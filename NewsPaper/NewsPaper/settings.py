82ea9ee09d000b8395faee42eaec4a953d889f3c
VetN <122635397+VetN@users.noreply.github.com>
2023-07-05 10:56:27 +0300

    add

--- NewsPaper/NewsPaper/settings.py
+++ NewsPaper/NewsPaper/settings.py
@@ -42,9 +42,12 @@ INSTALLED_APPS = [
     'django.contrib.sites',
 	'django.contrib.flatpages',
 
+    
+
     'news',
     'account',
     'static',
+    
 ]
 
 MIDDLEWARE = [
@@ -124,12 +127,15 @@ SITE_ID = 1
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = 'static/'
+STATICFILES_DIRS = [BASE_DIR / "static"]
+
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
-STATICFILES_DIRS = [BASE_DIR / "static"]
+
 
 MEDIA_ROOT = os.path.join(BASE_DIR / 'media')
 MEDIA_URL = 'media/'
---
+++ NewsPaper/news/templatetags/custom_filters.py
@@ -0,0 +1,24 @@
+from django import template
+import os
+
+list = os.path.join('bad_words.txt')
+with open(list, 'r', encoding='utf-8') as file:
+    list_badwords = [word.rstrip('\n') for word in file.readlines()]
+   
+
+
+
+register = template.Library()
+
+@register.filter(name='censor')
+
+def censor(textUser):
+    for w in list_badwords:
+        textUser = textUser.replace(w, "(слово удалено-проверка фильтром censor)")
+    return textUser
+
+  
+
+
+
+
