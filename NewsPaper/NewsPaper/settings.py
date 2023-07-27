c0741ccd3eea1afe53a476f468f8354e7d15eae5
VetN <122635397+VetN@users.noreply.github.com>
2023-07-27 08:45:45 +0300

    post

--- NewsPaper/NewsPaper/settings.py
+++ NewsPaper/NewsPaper/settings.py
@@ -186,7 +186,7 @@ ACCOUNT_FORMS = {'signup': 'news.forms.CommonSignupForm',
 EMAIL_HOST = 'smtp.yandex.ru' # адрес сервера Яндекс-почты для всех один и тот же
 EMAIL_PORT = 465 # порт smtp сервера тоже одинаковый
 EMAIL_HOST_USER = 'vet.ness' # ваше имя пользователя, например если ваша почта user@yandex.ru, то сюда надо писать user, иными словами, это всё то что идёт до собаки
-EMAIL_HOST_PASSWORD = 'iwillbetheBest' # пароль от почты
+EMAIL_HOST_PASSWORD =  # пароль от почты
 EMAIL_USE_SSL = True # Яндекс использует ssl, подробнее о том, что это, почитайте на Википедии, но включать его здесь обязательно
 
 
